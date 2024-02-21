7a1d0cadfa8f974070620ace435d5272754d300a
Max Alva <alva85@gmail.com>
2024-02-21 06:41:41 -0500

    Menú lateral

--- src/app/presentation/layouts/dashboard-layout/dashboard-layout.component.ts
+++ src/app/presentation/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -1,19 +1,22 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { routes } from '../../../app.routes';
+import { SidebarMenuItemComponent } from '../../components/sidebar-menu-item/sidebar-menu-item.component';
 
-@Component({
+@Component( {
   selector: 'app-dashboard-layout',
   standalone: true,
   imports: [
     RouterOutlet,
+    SidebarMenuItemComponent,
   ],
   templateUrl: './dashboard-layout.component.html',
   styleUrl: './dashboard-layout.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
-})
-export class DashboardLayoutComponent  {
+  changeDetection: ChangeDetectionStrategy.OnPush,
+} )
+export class DashboardLayoutComponent {
 
-
+  routes = routes[ 0 ].children?.filter( route => route.data );
 
 
 }
