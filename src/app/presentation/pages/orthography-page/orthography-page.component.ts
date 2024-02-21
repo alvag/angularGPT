d4a3d5c645f2f48cedb8057b5462bf0eeb9a3847
Max Alva <alva85@gmail.com>
2024-02-21 06:52:51 -0500

    Burbujas de chat

--- src/app/presentation/pages/orthography-page/orthography-page.component.ts
+++ src/app/presentation/pages/orthography-page/orthography-page.component.ts
@@ -1,13 +1,18 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChatMessageComponent } from '../../components/chat-bubbles/chat-message/chat-message.component';
+import { MyMessageComponent } from '../../components/chat-bubbles/my-message/my-message.component';
 
-@Component({
+@Component( {
   selector: 'app-orthograpy-page',
   standalone: true,
-  imports: [],
+  imports: [
+    ChatMessageComponent,
+    MyMessageComponent,
+  ],
   templateUrl: './orthography-page.component.html',
   styleUrl: './orthography-page.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
-})
+  changeDetection: ChangeDetectionStrategy.OnPush,
+} )
 export default class OrthographyPageComponent {
 
 }
